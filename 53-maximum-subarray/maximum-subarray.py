cf94133cc373cea7014ae1eceeee30dbe8d456e2
Ashish Singh <260835087+ashsingh6294@users.noreply.github.com>
2026-06-30 02:48:05 +0530

    Time: 19 ms (96.71%) | Memory: 31.5 MB (19.67%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,5 +1,6 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        
         curr_sum = 0
 
         max_sum = nums[0]
@@ -8,7 +9,8 @@ class Solution:
             curr_sum += nums[i]
             if curr_sum > max_sum:
                 max_sum = curr_sum
+
             if curr_sum < 0:
                 curr_sum = 0
-        
+
         return max_sum
